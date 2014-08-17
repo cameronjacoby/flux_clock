e3d3d647c2dd3bd004beb8e3b19b808cf231ab69
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-17 01:25:31 -0700

    color scale working

--- script.js
+++ script.js
@@ -77,7 +77,6 @@ $(document).ready(function() {
     var makeHourHand = function() {
       makeNumbers();
       hour = hour >= 12 ? hour - 12 : hour;
-      console.log(hour);
       var theta = (30 * Math.PI / 180),
         x = center + 250 * Math.cos((hour + (min / 60) + (sec / 3600)) * theta - Math.PI / 2),
         y = center + 250 * Math.sin((hour + (min / 60) + (sec / 3600)) * theta - Math.PI / 2);
@@ -94,61 +93,207 @@ $(document).ready(function() {
       makeSecHand();
     };
 
-    // 0 (20.25, 21.5, 58.75)
-
-    // 1 dark blue (4, 5, 13)
-    // 1 dark orange (15, 6, 5) ***
-
-    // 2 (63, 24.75, 20.5)
-
-    // 3 (111, 43.5, 36)
-
-    // 4 (159, 62.25, 51.5)
-
-    // 5 (207, 81, 67)
-
-    // 6 bright orange (255, 100, 83)
-
-    // 7 (255, 122, 85.5)
-
-    // 8 (255, 144, 88)
-
-    // 9 (255, 166, 90.5)
-
-    // 10 (255, 188, 93)
-
-    // 11 light gold (255, 210, 95)
-
-    // 12 (255, 229.5, 164.6)
-
-    // 13 pale yellow (255, 249, 234)
-
-    // 14 (227, 244, 244.5)
-
-    // 15 pale blue (199, 239, 255)
-
-    // 16 (121.5, 208, 255)
-
-    // 17 bright blue (44, 177, 255)
-
-    // 18 (72, 130, 255)
-
-    // 19 blue-purple (100, 83, 255)
-
-    // 20 (84.5, 77, 225.5)
-
-    // 21 blue (69, 71, 196)
-
-    // 22 (52.75, 54.5, 150.25)
-
-    // 23 (36.5, 38, 104.5)
-
     var changeColor = function() {
-      console.log(hour, min, sec);
-      var r = 100 - 0.5 * sec,
-        g = 83 - 0.25 * sec,
-        b = 255 - sec;
-      body.css("background-color", "rgb(" + r + ", " + g + ", " + b + ")"); 
+      var r,
+        g,
+        b;
+
+      var modCheck = function() {
+        if (min % 4 === 0) {
+          min = min;
+        }
+        else if (min % 2 === 0) {
+          min = min + 2;
+        }
+        else if (min % 3 === 0) {
+          if (min === 9 || min === 21 || min === 33 || min === 45 || min === 57) {
+            min = min + 3;
+          }
+          else {
+            min = min + 1;
+          }
+        }
+        else if (min % 1 === 0) {
+          if (min === 7 || min === 11 || min === 19 || min === 23 || min === 31 ||
+            min === 35 || min === 47 || min === 43 || min === 55 || min === 59) {
+              min = min + 1;
+          }
+          else {
+
+            min = min + 3;
+          }
+        }
+      };
+
+      if (hour < 1) {
+        modCheck();
+        r = 20 - 0.25 * min;
+        g = 22 - 0.25 * min;
+        b = 59 - 0.75 * min;
+      }
+
+      else if (hour < 2) {
+        modCheck();
+        r = 15 + 0.75 * min;
+        g = 6 + 0.25 * min;
+        b = 5 + 0.25 * min;
+      }
+
+      else if (hour < 3) {
+        modCheck();
+        r = 63 + 0.75 * min;
+        g = 25 + 0.25 * min;
+        b = 21 + 0.25 * min;
+      }
+
+      else if (hour < 4) {
+        modCheck();
+        r = 111 + 0.75 * min;
+        g = 44 + 0.25 * min;
+        b = 36 + 0.25 * min;
+      }
+
+      else if (hour < 5) {
+        modCheck();
+        r = 159 + 0.75 * min;
+        g = 62 + 0.25 * min;
+        b = 52 + 0.25 * min;
+      }
+
+      else if (hour < 6) {
+        modCheck();
+        r = 207 + 0.75 * min;
+        g = 81 + 0.25 * min;
+        b = 67 + 0.25 * min;
+      }
+
+      else if (hour < 7) {
+        modCheck();
+        r = 255;
+        g = 100 + 0.25 * min;
+        b = 83;
+      }
+
+      else if (hour < 8) {
+        modCheck();
+        r = 255;
+        g = 122 + 0.25 * min;
+        b = 86;
+      }
+
+      else if (hour < 9) {
+        modCheck();
+        r = 255;
+        g = 144 + 0.25 * min;
+        b = 88;
+      }
+
+      else if (hour < 10) {
+        modCheck();
+        r = 255;
+        g = 166 + 0.25 * min;
+        b = 91;
+      }
+
+      else if (hour < 11) {
+        modCheck();
+        r = 255;
+        g = 188 + 0.25 * min;
+        b = 93;
+      }
+
+      else if (hour < 12) {
+        modCheck();
+        r = 255;
+        g = 210 + 0.25 * min;
+        b = 95 + 1.25 * min;
+      }
+
+      else if (hour < 13) {
+        modCheck();
+        r = 255;
+        g = 230 + 0.25 * min;
+        b = 165 + 1.25 * min;
+      }
+
+      else if (hour < 14) {
+        modCheck();
+        r = 255 - 0.5 * min;
+        g = 249;
+        b = 234 + 0.25 * min;
+      }
+
+      else if (hour < 15) {
+        modCheck();
+        r = 227 - 0.5 * min;
+        g = 244;
+        b = 245 + 0.25 * min;
+      }
+
+      else if (hour < 16) {
+        modCheck();
+        r = 199 - 1.25 * min;
+        g = 239 - 0.5 * min;
+        b = 255;
+      }
+
+      else if (hour < 17) {
+        modCheck();
+        r = 122 - 1.25 * min;
+        g = 208 - 0.5 * min;
+        b = 255;
+      }
+
+      else if (hour < 18) {
+        modCheck();
+        r = 44 + 0.5 * min;
+        g = 177 - 0.75 * min;
+        b = 255;
+      }
+
+      else if (hour < 19) {
+        modCheck();
+        r = 72 + 0.5 * min;
+        g = 130 - 0.75 * min;
+        b = 255;
+      }
+
+      else if (hour < 20) {
+        modCheck();
+        r = 100 - 0.25 * min;
+        g = 83;
+        b = 255 - 0.5 * min;
+      }
+
+      else if (hour < 21) {
+        modCheck();
+        r = 85 - 0.25 * min;
+        g = 77;
+        b = 226 - 0.5 * min;
+      }
+
+      else if (hour < 22) {
+        modCheck();
+        r = 69 - 0.25 * min;
+        g = 71 - 0.25 * min;
+        b = 196 - 0.75 * min;
+      }
+
+      else if (hour < 23) {
+        modCheck();
+        r = 53 - 0.25 * min;
+        g = 55 - 0.25 * min;
+        b = 150 - 0.75 * min;
+      }
+
+      else {
+        modCheck();
+        r = 37 - 0.25 * min;
+        g = 38 - 0.25 * min;
+        b = 105 - 0.75 * min;
+      }
+
+      body.css("background-color", "rgb(" + r + ", " + g + ", " + b + ")");
     };
 
     var getTime = function() {
